b29f66ff20c79b209facbe5f4954267be5dc6b73
ionc21 <ionc86@gmail.com>
2017-08-06 21:55:36 +0200

    Initialize repository

---
+++ src/main/java/com/pgs/ApacheCollections4Application.java
@@ -0,0 +1,12 @@
+package com.pgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCollections4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCollections4Application.class, args);
+	}
+}
